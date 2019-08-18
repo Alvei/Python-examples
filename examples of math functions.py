e2d36e95149467570363b681478f152bf1887c40
Hugo Sarrazin <Hugo_Sarrazin@mckinsey.com>
2019-08-17 20:36:31 -0700

    clean-up

--- examples of math functions.py
+++ examples of math functions.py
@@ -1,49 +1,64 @@
-# -*- coding: utf-8 -*-
 """
 Created on Sat Jan 10 16:53:39 2015
 
 @author: Hugo Sarrazin
+Python 3.X
 """
 
-def findDivisors(n1, n2):
-    """ Assumes that n1 and n2 are positive ints
-        Returns a tuple containing all common divisors of n1 & n2 """
-        
-    divisors = ()   #The empty tuple
- 
-    for i in range(1, min(n1, n2) +1):
-        # print "i =", i
-        if n1%i == 0 and n2%i == 0:
-            # print "found divisor = ", i
-            divisors = divisors + (i,)
-    return divisors
-        
-# Test harness
-#divisors = findDivisors(20, 100)
-#print divisors
-#total = 0
-#for d in divisors:
-#    total += d
-#print total
 
-def findExtremeDivisors(n1, n2):
-    """ Assumes that n1 and n2 are positive ints
-        Returns a tuple containing the smallest commondivisors > 1
-        and the largest common divisors of n1 & n2 """
-    divisors = ()
+def findDivisors(number1, number2):
+    """ Find the common divisors of number1 and number2
+        Assumes that number1 and number2 are positive ints
+        Returns a tuple containing all common divisors
+        Signature: (int, int) -> tuple."""
+
+    assert number1 >= 1 and number2 >= 1
+
+    divisors = ()   # Initialize the empty tuple
+
+    for num in range(1, min(number1, number2) + 1):
+        # print("num = ", num)
+        if number1 % num == 0 and number2 % num == 0:
+            # print("found divisor = ", num)
+            divisors = divisors + (num,)
+    return divisors
+
+
+# Test harness for divisor
+def testDivisors():
+    divisors = findDivisors(20, 100)
+    print("Answer is:\t\t\t", divisors)
+    print("Answer should be:\t (1, 2, 4, 5, 10, 20)")
+
+    total = 0
+    for digit in divisors:
+        total += digit
+    print("Sum of divisors:\t", total)
+    print("Answer should be:\t 42")
+
+
+def findExtremeDivisors(number1, number2):
+    """ Find the smallest and largest common divisors of number1 and number2.
+        Assumes that number1 and number2 are positive ints.
+        Signature (int, int) => (tuple)."""
+
+    assert number1 >= 1 and number2 >= 1
+
     minVal, maxVal = None, None
-    
-    for i in range(2, min(n1, n2) + 1): 
-              # print "i =", i
-        if n1%i == 0 and n2%i == 0:
+
+    for i in range(2, min(number1, number2) + 1):
+        # print("i =", i)
+        if number1 % i == 0 and number2 % i == 0:
             if minVal == None or i < minVal:
                 minVal = i
             if maxVal == None or i > maxVal:
                 maxVal = i
-                
+
     return (minVal, maxVal)
 
+
 # Test harness
-minDiv, maxDiv = findExtremeDivisors(100,200)  
-print "Min Divisor =", minDiv  
-print "Max Divisor =", maxDiv  
+testDivisors()
+minDiv, maxDiv = findExtremeDivisors(100, 200)
+print("Min Divisor =", minDiv, "\t=> Answer = 2")
+print("Max Divisor =", maxDiv, "\t=> Answer = 100")
