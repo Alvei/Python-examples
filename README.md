8cbef4ddc2067dbcb556ca81a7840ec60d7698d5
Hugo Sarrazin <Hugo_Sarrazin@mckinsey.com>
2019-08-18 12:12:34 -0700

    Updated to python 3

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Python-examples
 This is a repository to experiment with short python examples
 It is mostly created in python 3.X
+
+*examples of decorators* with simple math operations
+*examples of classes* to show class formats and inheritance
--- examples of error checking.py
+++ examples of error checking.py
@@ -2,25 +2,26 @@
 """
 Created on Mon Dec 29 13:47:08 2014
 
-Simple function to read input with some error checking
+Simple functions to read input with some error checking.
+Note that checking for string does not work since it assume a 
+number is a string
 python 3.x
 """
 
 
-def readInt():
+def read_int():
     """ Function to ask for integer with basic error handling.
         Signature: (None) -> Int or error."""
     while True:
-        print("Hi")
+        val = input('Enter an integer: ')
         try:
-            val = input('Enter an integer: ')
             val = int(val)
             return val
         except ValueError:
-            print(val), 'is not an integer'
+            print(val, 'is not an integer')
 
 
-def readVal(valType, requestMsg, errorMsg, maxTries=4):
+def read_val(val_type, request_msg, error_msg, max_tries=4):
     """ Function to test the input with error handling
     Input:
         valtype: is the type of value expected (eg., int)
@@ -28,33 +29,36 @@ def readVal(valType, requestMsg, errorMsg, maxTries=4):
         errorMsg: str to be displayed to user when error happens
     """
 
-    numTries = 0
-    while numTries < maxTries:
-        val = input(requestMsg)
+    num_tries = 0
+    while num_tries < max_tries:
+        val = input(request_msg)
         try:
-            val = valType(val)
+            val = val_type(val)
             return val
         except ValueError:  # Handler for the exception
-            print(errorMsg)
-            numTries += 1
+            print(error_msg)
+            num_tries += 1
 
     raise TypeError('Number of tries exceeded')
 
 
-# Test Harness
-# #############
-readInt()
-# print(readVal(int, "Enter int: ", "Not an int."))
-# print(readVal(str, "Enter string: ", "Not an string."))
-
-# Define a function here.
-
-
 def temp_convert(var):
+    """ """
     try:
         return int(var)
     except ValueError:
         print("The argument does not contain integers\n")
 
-# Call above function here.
-# temp_convert("xyz");
+
+# Test Harness
+# #############
+def main():
+    """ Main code """
+    # read_int()
+    print(read_val(int, "Enter int: ", "Not an int."))
+    print(read_val(str, "Enter string: ", "Not an string."))
+
+    # temp_convert("xyz");
+
+if __name__ == '__main__':
+    main()
