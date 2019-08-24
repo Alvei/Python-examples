1e51fc9de6add5f909d5d71d3504826eba20321f
alvei <hugo_sarrazin@yahoo.com>
2019-08-24 11:22:00 -0700

    Created pw.py

---
+++ Automate_boring_stuff/pw.py
@@ -0,0 +1,19 @@
+""" Password Locker.
+    An insecured password locker program. Works from the command line.
+    Inspired from Automate the Boring Stuff with Python - Al Sweigart. Chapter 6. Page 137.
+    Signature: (string, string) -> None
+""" 
+import sys, pprint, random
+passwd = {'Bob': 'yikes!'}
+
+
+if len(sys.argv) < 2:
+    print("Usage: Python pw.py [account] - copy account password")
+    sys.exit()
+
+account = sys.argv[1]       # First command line arg is the account name.
+
+# Very insecure. Only doing to use setdefault. It uses a random integer generator. 
+print("For account ", account), "the pwd is ", passwd.setdefault(account, random.randint(1000,10000))
+
+print(passwd)
