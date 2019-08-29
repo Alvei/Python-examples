2bda6d266a1af22a39af262054a8b65dbaa6861d
alvei <hugo_sarrazin@yahoo.com>
2019-08-29 07:39:02 -0700

    Added a memoization with functools

--- Recursion/Etudes_Fibonacci.py
+++ Recursion/Etudes_Fibonacci.py
@@ -2,12 +2,30 @@
    Compares the recursive implementation of Fib with the one using
    Hashing and dynamic programing
    Python 3.5"""
+from functools import lru_cache
+
+
+def fib_loop(num):
+    """ Calculates the fibonacci number of num.
+        Use loops and double assignments.
+        Signature (int) -> int """
+    assert num >= 0 and isinstance(num, int)
+
+    old, new = 0, 1
+    for _ in range(num):
+        old, new = new, old + new
+    return old
 
 
 def fib(num):
     """Calculates the fibonacci number of num.
        Signature (int) -> int """
-    assert num >= 0 and isinstance(num, int)
+
+    # Check that the input is a positive integer
+    if not isinstance(num, int):
+        raise TypeError("num must be an int. It is", type(num))
+    if num < 0:
+        raise ValueError("num must be positive. It is", num)
 
     # Base case
     if num in (0, 1):
@@ -34,17 +52,19 @@ def fast_fib(num, memo={}):
         memo[num] = result
         return result
 
+@lru_cache(maxsize = 1000)
+def fast_fib2(num):
+    """ Implementation of recursive fib that does not calculate the same fib
+        number. It uses functools to do memoization.
+        Signature (int, dict) -> int """
 
-def fib_loop(num):
-    """ Calculates the fibonacci number of num.
-        Use loops and double assignments.
-        Signature (int) -> int """
-    assert num >= 0 and isinstance(num, int)
+    if not isinstance(num, int):
+        raise TypeError("Expecting int but got", type(int))
+    if num in (0,1):
+        return 1
+    else:
+        return fast_fib2(num-1) + fast_fib2(num-2)
 
-    old, new = 0, 1
-    for _ in range(num):
-        old, new = new, old + new
-    return old
 
 def fib_yield(num):
     """ Calculates the fibonacci number of num.
@@ -52,7 +72,7 @@ def fib_yield(num):
         Signature (int) -> int """
     yield 0  # special case
 
-    if num > 0: 
+    if num > 0:
         yield 1  # special case
     last = 0  # initially set to fib(0)
     next = 1  # initially set to fib(1)
@@ -64,32 +84,32 @@ def test_fib(num):
     """ Test harness. The last call with n = 35 is instanteneous
         for fastFib but takes long for fib """
 
+    print("Traditional \t Memoization ")
     for i in range(num + 1):
-        print('fib of ', i, ' = ', str(fib(i)))
-
-    for j in range(num + 1):
-        print('fast_fib of ', j, ' = ', str(fast_fib(j)))
+        print(i, ':', str(fib(i)), "\t\t", str(fast_fib(i)))
 
 
 def test_fib2(num):
     """ Test harness. The last call with n = 35 is instanteneous
         for fastFib but takes long for fib """
     print('fastFib of \t', num, ' = ', str(fast_fib(num)))   # Should be super fast
-    print('fib of \t\t', num, ' = ', str(fib(num)))            # Should take some time
+    print('fastFib2 of \t', num, ' = ', str(fast_fib2(num))) # Should be super fast
+    print("This should take a while....")
+    print('fib of \t\t', num, ' = ', str(fib(num)))          # Should take some time
 
 
 def main():
     """ Main code """
     print("Fib using loop F(45):", fib_loop(45))
 
-    print('\n\n')
+    print('\n')
     test_fib(4)
-    print('\n\n')
+    print('\n')
     test_fib2(35)
 
     # This is still buggy
-    for index in fib_yield(35):
-        print(index)
+    #for index in fib_yield(35):
+    #    print(index)
     #print("Using Yield:", fib_yield(10))
 
 
