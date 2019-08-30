c4996c0b7340e9f3e89d9c0c286ef1a7be948123
alvei <hugo_sarrazin@yahoo.com>
2019-08-30 16:03:09 -0700

    Simplified unittest loops

--- UnitTest/test_circle.py
+++ UnitTest/test_circle.py
@@ -6,9 +6,9 @@ class TestCircleArea(unittest.TestCase):
     """ Simple unit test. Run python -m unittest"""
     def test_area(self):
         # Test areas with radius > 0
-        self.assertAlmostEqual(circle_area(1), pi)
-        self.assertAlmostEqual(circle_area(0), 0)
-        self.assertAlmostEqual(circle_area(2.1), pi * 2.1**2)
+        test_dict = {1: pi, 0: 0, 2.1: pi*2.1**2}
+        for key, value in test_dict.items():
+            self.assertAlmostEqual(circle_area(key), value)
 
     def test_values(self):
         #  Make sure reasonable value are accepted
@@ -16,6 +16,10 @@ class TestCircleArea(unittest.TestCase):
 
     def test_types(self):
         # Test for different types
-        self.assertRaises(TypeError, circle_area, True)
-        self.assertRaises(TypeError, circle_area, 2+3j)
-        self.assertRaises(TypeError, circle_area, "one")
+        test_list = [True, 2+3j, "one"]
+        for value in test_list:
+            self.assertRaises(TypeError, circle_area, value)
+
+
+if __name__ == "__main__":
+    unittest.main()
