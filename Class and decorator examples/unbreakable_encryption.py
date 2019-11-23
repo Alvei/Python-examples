0bdf71c18beb92c2c8efcdff570d0f99212ca68d
Alvei <hugo_sarrazin@yahoo.com>
2019-11-23 07:34:40 -0800

    Clean-up type for decrypted and a few comments

--- Class and decorator examples/unbreakable_encryption.py
+++ Class and decorator examples/unbreakable_encryption.py
@@ -1,22 +1,27 @@
 """ unbreakable_encryption.py
-    One-time pad is a way to encrypt data by combining with meaningless
-    random dummy data. Alot of the operations are done in bit-strings and
-    using XOR since very efficient. bit-string are saved in int.
-    Inspired by David Kopec Chapter 1. """
+    One-time pad is a way to encrypt data by combining with
+    meaningless random dummy data.
+    str are coverted to bits first using .encode() and .decore().
+    Then bit-string are saved in int. with .from_bytes() and .to_bytes()
+    Alot of the operations are done in bit-strings using XOR
+    bit-string XOR since very efficient. Taking advantage of XOR property.
+                A^B = C then C^B = A and C^A = B.
+    Inspired by David Kopec Chapter 1. 
+    also good reading at https://www.devdungeon.com/content/working-binary-data-python """
 
 from secrets import token_bytes   # pseudo-random data generating function
 from typing import Tuple
 
 def random_key(length: int) -> int:
     """ Generate length random bytes using token-bytes function. """
-    tb: bytes = token_bytes(length)
+    randon_bytes: bytes = token_bytes(length)
 
     # Convert those bytes into a bit string store in (type) int and return it
     # (type) int can be used for generic bit string and can be of arbitrary length
-    return int.from_bytes(tb, "big")
+    return int.from_bytes(randon_bytes, "big")
 
-def encrypt (original: str) -> Tuple[int, int]:
-    """ Encrypts the string by taking advantage of XOR property.
+def encrypt(original: str) -> Tuple[int, int]:
+    """ Encrypts the string by taking advantage of bit-string XOR property.
         A^B = C then C^B = A and C^A = B.
         Returns the dummy data and encrypted data."""
 
@@ -33,7 +38,9 @@ def encrypt (original: str) -> Tuple[int, int]:
 
 def decrypt(key1: int, key2: int) -> str:
     """ Decrypt the code where key1 is the dummy data and key2 is the encrypted str. """
-    decrypted: int = key1 ^ key2   # Using XOR
+
+    # Taking advantage of XOR property. A^B = C then C^B = A and C^A = B.
+    decrypted: int = key1 ^ key2
 
     # Convert the bit-string integer back to bytes
     # Need to add 7 before the int division (//) by 8 to ensure round-up
@@ -43,6 +50,7 @@ def decrypt(key1: int, key2: int) -> str:
     return temp.decode()
 
 if __name__ == "__main__":
-    key1, key2 = encrypt("One time pad!")
-    result: str = decrypt(key1, key2)
+    random_key, code = encrypt("One time pad!")
+    # print(f"\n=>{type(random_key)} {random_key}, {code}")
+    result: str = decrypt(random_key, code)
     print(f"\n{result}")
