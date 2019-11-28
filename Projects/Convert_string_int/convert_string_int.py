c49dd488535ad8bd7ad05ebb76f6aed2129e478e
Alvei <hugo_sarrazin@yahoo.com>
2019-11-28 14:20:35 -0800

    Moved the return try outside for loop

--- Projects/Convert_string_int/convert_string_int.py
+++ Projects/Convert_string_int/convert_string_int.py
@@ -1,5 +1,6 @@
 """ Convert String numbers to int. """
 from typing import Union
+
 def convert_to_int(integer_string_with_commas: str) -> Union[int, None]:
     """ Converts numerical strings to become int. It handles various cases with punctuation.
         Returns None when something unusual happens. """
@@ -7,18 +8,21 @@ def convert_to_int(integer_string_with_commas: str) -> Union[int, None]:
 
     comma_separated_parts: list = integer_string_with_commas.split(",")
     for part in comma_separated_parts:
-        print(comma_separated_parts)
+
         # Number of special cases
         if len(part) > 3:
             return None
         if part != 0 and len(part) != 3: # Skip over the 1st part since it could have 1 or 2
             return None
+        #if "." in part:   #  More readable than the try/except
+        #    return None
 
-        # Create a single string by combining the parts
-        integer_string_without_commas = "".join(comma_separated_parts)
+    # Create a single string by combining the parts
+    integer_string_without_commas: str = "".join(comma_separated_parts)
+    #return int(integer_string_without_commas)
 
-        # Try, it will not work if you have a decimal still in the string
-        try:
-            return int(integer_string_without_commas)
-        except ValueError:
-            return None
+    # Try, it will not work if you have a decimal still in the string
+    try:
+        return int(integer_string_without_commas)
+    except ValueError:
+        return None
