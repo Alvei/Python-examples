b209f9500b9e103ec1f07f883106427048026257
Alvei <hugo_sarrazin@yahoo.com>
2019-11-28 22:24:45 -0800

    Added the timing of each implementation

--- Recursion/Fibonacci.py
+++ Recursion/Fibonacci.py
@@ -1,24 +1,22 @@
-"""Fibonacci.py
-   Compares the recursive implementation of Fib with the one using
-   Hashing and dynamic programing. """
+""" Fibonacci.py
+    Compares the recursive implementation of Fib
+    fib1() is basic recursion.
+    fib2() is using a while loop.
+    fib3() is using yield.
+    fib4() is  using dict to create memoization.
+    fib5() is using lru_cache from functools to do memoization.
+    fib6() is using lru_cache from functools to do memoization. Using tuples.
+    fibt7() is a manual memoization.
+    fib6 > fib2 > fib5 > fib7 > fib1
+    The tuple memoization is best. Recursion is worst.
+    Looping (fib2) is suprisingly fast. LRU_cach or dict implementation is similar
+    My implementation of LRU_cache with decorator is pretty bad. Probably the sorting and conversion to tuple.
+    Good article at https://fulmicoton.com/posts/fibonacci/  """
 from functools import lru_cache
-from typing import Generator, Dict
+import time
+from typing import Generator, Dict, Tuple
 
-
-def fib_loop(num: int) -> int:
-    """ Calculates the fibonacci number of num.
-        Use loops and double assignments. """
-
-    assert isinstance(num, int)
-    assert num >= 0
-
-    old, new = 0, 1
-    for _ in range(num):
-        old, new = new, old + new
-    return old
-
-
-def fib(num: int) -> int:
+def fib1(num: int) -> int:
     """Calculates the fibonacci number of num. """
 
     # Check that the input is a positive integer
@@ -30,44 +28,32 @@ def fib(num: int) -> int:
     # Base case
     if num < 2:
         return num
-    return fib(num - 1) + fib(num - 2)
+    return fib1(num - 1) + fib1(num - 2)
 
+def fib2(num: int) -> int:
+    """ Calculates the fibonacci number of num.
+        Use loops and double assignments.
+        Has linear computational time and constant memory. """
 
-def fast_fib(num: int, memo: Dict[int, int] = {}) -> int:
-    """ Implementation of recursive fib that does not calculate the same fib
-        number. It uses a dictionary of previously computed Fib numbers.
-        dictionary entry in memo => (key = num, value = fib(num))
-        Initialize memo if empty. """
-
-    assert num >= 0 and isinstance(num, int)
-
-    # Base case
-    if num < 2:
-        return num
-    try:  # Check to see if already computed and in dictionary
-        return memo[num]
-    except KeyError:  # Otherwise calculate the new {key, value} pair
-        result = fast_fib(num - 1, memo) + fast_fib(num - 2, memo)
-        memo[num] = result
-        return result
-
-@lru_cache(maxsize=1000)
-def fast_fib2(num: int) -> int:
-    """ Implementation of recursive fib that does not calculate the same fib
-        number. It uses functools to do memoization. """
-
+    # Check that the input is a positive integer
     if not isinstance(num, int):
-        raise TypeError("Expecting int but got", type(int))
+        raise TypeError("num must be an int. It is", type(num))
     if num < 0:
         raise ValueError("num must be positive. It is", num)
 
-    if num < 2:
-        return num
-    return fast_fib2(num-1) + fast_fib2(num-2)
+    old, new = 0, 1
+    for _ in range(num):
+        old, new = new, old + new
+    return old
 
-
-def fib_yield(num: int) -> Generator:
+def fib3(num: int) -> Generator:
     """ Calculates the fibonacci number of num. Use generator. """
+    # Check that the input is a positive integer
+    if not isinstance(num, int):
+        raise TypeError("num must be an int. It is", type(num))
+    if num < 0:
+        raise ValueError("num must be positive. It is", num)
+
     yield 0  # special case
 
     if num > 0:
@@ -79,37 +65,143 @@ def fib_yield(num: int) -> Generator:
         last, next_up = next_up, last + next_up
         yield next_up  # main generation step
 
+def fib4(num: int, memo: Dict[int, int] = {}) -> int:
+    """ Implementation of recursive fib that does not calculate the same fib
+        number. It uses a dictionary of previously computed Fib numbers.
+        dictionary entry in memo => (key = num, value = fib(num))
+        Initialize memo if empty. """
+
+    # Check that the input is a positive integer
+    if not isinstance(num, int):
+        raise TypeError("num must be an int. It is", type(num))
+    if num < 0:
+        raise ValueError("num must be positive. It is", num)
+
+    # Base case
+    if num < 2:
+        return num
+    try:  # Check to see if already computed and in dictionary
+        return memo[num]
+    except KeyError:  # Otherwise calculate the new {key, value} pair
+        result = fib4(num - 1, memo) + fib4(num - 2, memo)
+        memo[num] = result
+        return result
+
+@lru_cache(maxsize=1000)
+def fib5(num: int) -> int:
+    """ Implementation of recursive fib that does not calculate the same fib
+        number. It uses functools to do memoization. """
+
+    if not isinstance(num, int):
+        raise TypeError("Expecting int but got", type(int))
+    if num < 0:
+        raise ValueError("num must be positive. It is", num)
+
+    if num < 2:
+        return num
+
+    return fib5(num-1) + fib5(num-2)
+
+@lru_cache(maxsize=None)
+def fib6(num: int) -> Tuple[int, int]:
+    """ Implementation of recursive fib that does not calculate the same fib
+        number. It uses functools to do memoization. By using tuples and doing 2 calculations
+        in every recursion and takes better advantage of memoization.
+        The first element [0] of tuple is the current answer. The second is F[n+1].
+        This implementation is very interesting. No reference for the algo. """
+
+    if not isinstance(num, int):
+        raise TypeError("Expecting int but got", type(int))
+    if num < 0:
+        raise ValueError("num must be positive. It is", num)
+
+    # Base case
+    if num == 0:
+        return 0, 1
+
+    else:
+        num_a, num_b = fib6(num // 2)
+        num_c = num_a * (num_b * 2 - num_a)
+        num_d = num_a * num_a + num_b * num_b
+
+        if num % 2 == 0:
+            return num_c, num_d
+        else:
+            return num_d, num_c + num_d
+
+def memoize(f):
+    """ Decorator for memoization function. """
+    cache = {}
+    def aux(*args, **kargs):
+        """ Dummy function. Dict are not hashable, transform kargs in a tuple of (key, value). """
+        key = (args, tuple(sorted(kargs.items())))
+
+        if key not in cache:
+            cache[key] = f(*args, **kargs)
+        return cache[key]
+    return aux
+
+@memoize
+def fib7(num: int) -> int:
+    """ Calculate Fib number. """
+
+    # Check that the input is a positive integer
+    if not isinstance(num, int):
+        raise TypeError("num must be an int. It is", type(num))
+    if num < 0:
+        raise ValueError("num must be positive. It is", num)
+
+    if num == 0:
+        return 0
+    if num == 1:
+        return 1
+    return fib7(num-1) + fib7(num-2)
+
 def test_fib(num: int) -> None:
     """ Test harness. The last call with n = 35 is instanteneous
         for fastFib but takes long for fib """
 
     print("Traditional \t Memoization ")
     for i in range(num + 1):
-        print(i, ':', str(fib(i)), "\t\t", str(fast_fib(i)))
+        print(i, ':', str(fib1(i)), "\t\t", str(fib3(i)))
 
 
 def test_fib2(num: int) -> None:
     """ Test harness. The last call with n = 35 is instanteneous
         for fastFib but takes long for fib """
-    print(f"fastFib of {num} =\t\t{str(fast_fib(num))}")   # Should be super fast
-    print(f"fastFib2 of {num} =\t{str(fast_fib2(num))}") # Should be super fast
+    print(f"fastFib of {num} =\t\t{str(fib3(num))}")   # Should be super fast
+    print(f"fastFib2 of {num} =\t{str(fib4(num))}") # Should be super fast
     print("This should take a while....")
-    print(f"Traditional recursion Fib {num} =\t {str(fib(num))}")  # Should take some time
-    print(f"Traditional loop Fib {num} =\t {str(fib_loop(num))}")  # Should take some time
+    print(f"Traditional recursion Fib {num} =\t {str(fib1(num))}")  # Should take some time
+    print(f"Traditional loop Fib {num} =\t {str(fib2(num))}")  # Should take some time
 
+def time_fib(func, num: int) -> int:
+    """ Simple function to calculate execution time. """
+    start = time.time()
+    answer = func(num)
+    end = time.time()
+    print(f"{func.__name__} took {(end-start)*1000:.2f} miliseconds")
+    return answer
 
 def main():
     """ Test Harness """
-
-    print('\n')
     #test_fib(4)
-    print('\n')
     #test_fib2(35)
-    test_fib(10)
+    #test_fib(10)
     # This is still buggy
     #for index in fib_yield(35):
     #    print(index)
     #print("Using Yield:", fib_yield(10))
+    num = 399
+    test_func = [fib7, fib5, fib4, fib2]  # Cannot time the yield and the tuple memoization
+    for func in test_func:
+        time_fib(func, num)
+
+    # Have to do manually since it has a tuple return value
+    start = time.time()
+    answer = fib6(num)
+    end = time.time()
+    print(f"{fib6.__name__} took {(end-start)*1000:.2f} miliseconds")
 
 
 if __name__ == '__main__':
--- Recursion/test_fibonacci.py
+++ Recursion/test_fibonacci.py
@@ -1,43 +1,107 @@
-from Fibonacci import fib, fib_loop, fast_fib, fast_fib2
+from Fibonacci import fib1, fib2, fib3, fib4, fib5, fib6, fib7
 import pytest
 
-def test_fib():
-    assert fib(0) == 0
-    assert fib(1) == 1
-    assert fib(2) == 1
-    assert fib(3) == 2
-    assert fib(6) == 8
-    assert fib(10) == 55
+def test_fib1():
+    """ Basic Fib recursion """
+    assert fib1(0) == 0
+    assert fib1(1) == 1
+    assert fib1(2) == 1
+    assert fib1(3) == 2
+    assert fib1(6) == 8
+    assert fib1(10) == 55
 
-    #while pytest.raises(TypeError):
-        #assert fib('a')
-    #while pytest.raises(ValueError):
-        #assert fib(-1)
+    with pytest.raises(TypeError):
+        assert fib1('a')
+    with pytest.raises(ValueError):
+        assert fib1(-1)
 
-def test_fib_loop():
-    assert fib_loop(0) == 0
-    assert fib_loop(1) == 1
-    assert fib_loop(2) == 1
-    assert fib_loop(3) == 2
-    assert fib_loop(6) == 8
-    assert fib_loop(10) == 55
+def test_fib2():
+    """ Basic Fib with loop """
+    assert fib2(0) == 0
+    assert fib2(1) == 1
+    assert fib2(2) == 1
+    assert fib2(3) == 2
+    assert fib2(6) == 8
+    assert fib2(10) == 55
 
-    """while pytest.raises(AssertionError):
-        assert fib_loop('a')
-        assert fib_loop(-1)"""
+    with pytest.raises(TypeError):
+        assert fib2('a')
+    with pytest.raises(ValueError):
+        assert fib2(-1)
 
-def test_fast_fib():
-    assert fast_fib(0) == 0
-    assert fast_fib(1) == 1
-    assert fast_fib(2) == 1
-    assert fast_fib(3) == 2
-    assert fast_fib(6) == 8
-    assert fast_fib(10) == 55
+""" NOT SURE HOW TO TEST A FUNCTION THAT RETURNS A GENERATOR
+def test_fib3():
+    assert fib3(0) == 0
+    assert fib3(1) == 1
+    assert fib3(2) == 1
+    assert fib3(3) == 2
+    assert fib3(6) == 8
+    assert fib3(10) == 55
 
-def test_fast_fib2():
-    assert fast_fib2(0) == 0
-    assert fast_fib2(1) == 1
-    assert fast_fib2(2) == 1
-    assert fast_fib2(3) == 2
-    assert fast_fib2(6) == 8
-    assert fast_fib2(10) == 55
+    with pytest.raises(TypeError):
+        assert fib2('a')
+    with pytest.raises(ValueError):
+        assert fib2(-1) """
+
+def test_fib4():
+    """ Uses dict to create memoization """
+    assert fib4(0) == 0
+    assert fib4(1) == 1
+    assert fib4(2) == 1
+    assert fib4(3) == 2
+    assert fib4(6) == 8
+    assert fib4(10) == 55
+
+    with pytest.raises(TypeError):
+        assert fib4('a')
+    with pytest.raises(ValueError):
+        assert fib4(-1)
+
+def test_fib5():
+    """ LRU Cache implementation of memoization. """
+    assert fib5(0) == 0
+    assert fib5(1) == 1
+    assert fib5(2) == 1
+    assert fib5(3) == 2
+    assert fib5(6) == 8
+    assert fib5(10) == 55
+
+    with pytest.raises(TypeError):
+        assert fib5('a')
+    with pytest.raises(ValueError):
+        assert fib5(-1)
+
+def test_fib6():
+    """ LRU Cache implementation with tuple. """
+    actual = fib6(0)
+    assert actual[0] == 0, f"Expected: 0, Actual: {actual[0]}"
+    actual = fib6(1)
+    assert actual[0] == 1, f"Expected: 1, Actual: {actual[0]}"
+    actual = fib6(2)
+    assert actual[0] == 1, f"Expected: 1, Actual: {actual[0]}"
+    actual = fib6(3)
+    assert actual[0] == 2, f"Expected: 2, Actual: {actual[0]}"
+    actual = fib6(6)
+    assert actual[0] == 8, f"Expected: 8, Actual: {actual[0]}"
+    actual = fib6(10)
+    assert actual[0] == 55, f"Expected: 55, Actual: {actual[0]}"
+
+    with pytest.raises(TypeError):
+        assert fib6('a')
+    with pytest.raises(ValueError):
+        assert fib6(-1)
+
+
+def test_fib7():
+    """ Manual implementation of memoization. """
+    assert fib7(0) == 0
+    assert fib7(1) == 1
+    assert fib7(2) == 1
+    assert fib7(3) == 2
+    assert fib7(6) == 8
+    assert fib7(10) == 55
+
+    with pytest.raises(TypeError):
+        assert fib7('a')
+    with pytest.raises(ValueError):
+        assert fib7(-1)
