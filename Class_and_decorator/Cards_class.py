b3c7afbb2199321d75f8ddce1eb9379994b520ca
alvei <hugo_sarrazin@yahoo.com>
2019-12-14 10:47:01 -0800

    Clean-up type hinting

--- Class_and_decorator/Cards_class.py
+++ Class_and_decorator/Cards_class.py
@@ -1,37 +1,44 @@
 """ Cards_class.py
     Each card is represented as a tuple of strings denoting the suit and rank.
-    The deck is represented as a list of cards. create_deck() creates a regular
-    deck of 52 playing cards, and optionally shuffles the cards. deal_hands() deals the deck of cards to four players.
+    Deck is represented as a list of cards. create_deck() creates a regular
+    deck of 52 playing cards, and optionally shuffles the cards.
+    deal_hands() deals the deck of cards to four players.
     Finally, play() plays the game.
     Inspired from https://realpython.com/python-type-checking/#duck-types-and-protocols """
 import random
-from typing import List
+from pprint import pprint
+from typing import List, Tuple
 
-""" Used Unicode glyphs like ♠ directly in the source code.
-    We could do this because Python supports writing source code in UTF-8 by default. """
+""" Used Unicode glyphs like ♠ directly in the source code because
+    Python supports writing source code in UTF-8 by default. """
 RANKS = '2 3 4 5 6 7 8 9 10 J Q K A'.split()
 SUITS = '♣ ♢ ♡ ♠'.split()
 
-def create_deck(shuffle: bool = False) -> List[tuple]:
-    """Create a new deck of 52 cards"""
-    deck = [(s, r)
-            for r in RANKS
-            for s in SUITS]
+def create_deck(shuffle: bool = False) -> List[Tuple[str, str]]:
+    """Create a new deck of 52 cards. """
+    deck = [(suit, rank)
+            for rank in RANKS
+            for suit in SUITS]
     if shuffle:
         random.shuffle(deck)
     return deck
 
-def deal_hands(deck):
+def deal_hands(deck: List[Tuple[str, str]]) -> Tuple[List[Tuple[str, str]], \
+        List[Tuple[str, str]], List[Tuple[str, str]], List[Tuple[str, str]]]:
     """Deal the cards in the deck into 4 hands"""
+    # list[start:end:stride], therefore deck[0::4], means start at 0 till the end stride=4
     return (deck[0::4], deck[1::4], deck[2::4], deck[3::4])
 
 def play():
     """Play a 4-player card game"""
     deck = create_deck(shuffle=True)
+
     names = "P1 P2 P3 P4".split()
-    hands = {n: h
-            for n, h in zip(names, deal_hands(deck))}
-    print(hands)
+
+    # Create a dictionary of each players hands
+    hands = {name: hand
+             for name, hand in zip(names, deal_hands(deck))}
+    pprint(hands)
 
 
 if __name__ == "__main__":
