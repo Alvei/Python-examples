37be8706c285fcd1974f31750f4504d099d6fe59
alvei <hugo_sarrazin@yahoo.com>
2019-12-26 10:43:49 -0800

    Implemented the Queue class

--- David_Kopec/generic_search.py
+++ David_Kopec/generic_search.py
@@ -1,43 +1,69 @@
 """ generic_search.py """
 from __future__ import annotations
-from typing import TypeVar, Iterable, Sequence, Any
+from typing import TypeVar, Generic
+from typing import List, Callable, Set, Optional
+#from typing import Iterable, Sequence, Any
+from typing import Deque, Dict
+#from heapq import heappush, heappop
 from typing_extensions import Protocol   # Need to install this module
-from typing import List, Callable, Set, Deque, Dict, Optional, Generic
-from heapq import heappush, heappop
 
 T = TypeVar('T')   # Create a generic type
 
 class Stack(Generic[T]):
-    """ Define a generic stack class. """
-
-    def __init___(self) -> None:
-        """ Initialize. """
+    """ Generic Stack class. """
+    def __init__(self) -> None:
         self._container: List[T] = []
 
     @property
     def empty(self) -> bool:
-        """ Not is true for empty container. """
+        """ Returns True for empty container. """
         return not self._container
 
     def push(self, item: T) -> None:
-        """ Add a Generic item to the stack. LIFO. """
+        """ Add to the end of the stack item(s). """
         self._container.append(item)
 
     def pop(self) -> T:
-        """Remove the last Generic item from the stack. LIFO. """
+        """ Remove the last item in the container. LIFO. """
         return self._container.pop()
 
     def __repr__(self) -> str:
-        """ Printing the stack. """
+        """ Use default repr(). """
+        return repr(self._container)
+
+class Queue(Generic[T]):
+    """ Generic Queue class. """
+    def __init__(self) -> None:
+        self._container: Deque[T] = Deque()
+
+    @property
+    def empty(self) -> bool:
+        """ Returns True for empty container. """
+        return not self._container
+
+    def push(self, item: T) -> None:
+        """ Add to the end of the stack item(s). """
+        self._container.append(item)
+
+    def pop(self) -> T:
+        """ Remove the 1st item in the container. FIFO. """
+        return self._container.popleft()
+
+    def __repr__(self) -> str:
+        """ Use default repr(). """
         return repr(self._container)
 
 class Node(Generic[T]):
-    """ Will keep track of how we got from one state to the other.
+    """ Will keep track of how we got from one state to the other (or one place to another).
         Node is a wrapper around the state. """
 
     def __init__(self, state: T, parent: Optional[Node], cost: float = 0.0,
                  heuristic: float = 0.0) -> None:
-        """ Initialize. """
+        """ Initializes Node().
+            state: Current state or current place.
+            parent: The previous Node or None.
+            cost: Used in the comparison operator used in *A search
+            heuristic: Used in the comparison operator used in *A search. """
         self.state: T = state
         self.parent: Optional[Node] = parent
         self.cost: float = cost
@@ -46,3 +72,92 @@ class Node(Generic[T]):
     def __lt__(self, other: Node) -> bool:
         """ Lower than comparator. """
         return (self.cost + self.heuristic) < (other.cost + other.heuristic)
+
+    def __str__(self) -> str:
+        """ printing. """
+        return f"state: {self.state} parent: {self.parent}"
+
+def node_to_path(node: Node[T]) -> List[T]:
+    """ Helps keep track of the path pursued. """
+    path: List[T] = [node.state]
+
+    # Work backwards from end to front
+    while node.parent is not None:
+        node = node.parent
+        path.append(node.state)
+    path.reverse()
+    return path
+
+def dfs(initial: T, goal_test: Callable[[T], bool],
+        successors: Callable[[T], List[T]]) -> Optional[Node[T]]:
+    """ Depth-First-Search algorithm. Uses Stack Data element.
+        initial: starting point for search.
+        goal_test: simple comparison fonction with input type T that returns a bool.
+        successors: function that returns a List[T] of all potential next steps or returns None. """
+
+    frontier: Stack[Node[T]] = Stack()      # Were we have yet to go of type Node[T]
+    frontier.push(Node(initial, None))      # Load-up the frontier with starting Node
+    explored: Set[T] = {initial}            # Where we have been. Use set to avoid duplication
+
+    print(f"Starting: {frontier}")
+    # Keep going while there is more to explore
+    while not frontier.empty:
+
+        # The 1st time, .pop() the starting Node, the other time, list
+        current_node: Node[T] = frontier.pop()
+        current_state: T = current_node.state
+
+        # If we found the goal, we are done!
+        if goal_test(current_state):
+            return current_node
+
+        # Check where we can go next and have not explored
+        for child in successors(current_state):
+            if child in explored: # Skip the children we already explored
+                continue
+
+            # Use the .add() method from set to the current child to the list of visited Nodes
+            explored.add(child)
+
+            # Load the stack with the current options -> child
+            # and keep track of parent -> current_node
+            frontier.push(Node(child, current_node))
+
+    return None     # Went through everything and never found goal
+
+def bfs(initial: T, goal_test: Callable[[T], bool],
+        successors: Callable[[T], List[T]]) -> Optional[Node[T]]:
+    """ Breadth-First-Search algorithm. Uses Queue Data element.
+        initial: starting point for search.
+        goal_test: simple comparison fonction with input type T that returns a bool.
+        successors: function that returns a List[T] of all potential next steps or returns None. """
+
+    frontier: Queue[Node[T]] = Queue()      # Were we have yet to go of type Node[T]
+    frontier.push(Node(initial, None))      # Load-up the frontier with starting Node
+    explored: Set[T] = {initial}            # Where we have been. Use set to avoid duplication
+
+    print(f"Starting: {frontier}")
+    # Keep going while there is more to explore
+    while not frontier.empty:
+
+        # The 1st time, .pop() the starting Node, the other time, list
+        current_node: Node[T] = frontier.pop()
+        current_state: T = current_node.state
+
+        # If we found the goal, we are done!
+        if goal_test(current_state):
+            return current_node
+
+        # Check where we can go next and have not explored
+        for child in successors(current_state):
+            if child in explored: # Skip the children we already explored
+                continue
+
+            # Use the .add() method from set to the current child to the list of visited Nodes
+            explored.add(child)
+
+            # Load the stack with the current options -> child
+            # and keep track of parent -> current_node
+            frontier.push(Node(child, current_node))
+
+    return None     # Went through everything and never found goal
--- David_Kopec/maze.py
+++ David_Kopec/maze.py
@@ -3,6 +3,7 @@ from enum import Enum
 import random
 from math import sqrt
 from typing import List, NamedTuple, Callable, Optional
+from generic_search import dfs, bfs, node_to_path, Node
 
 class Cell(str, Enum):
     """ Defined the different states each cell can have. """
@@ -14,17 +15,22 @@ class Cell(str, Enum):
 
 class MazeLocation(NamedTuple):
     """ Refer to individual location in the maze.
-        By using NamedTuple vs. Tuple we can access by key vs. index. """
+        By using NamedTuple vs. Tuple we can access by.attribute vs. index.
+        start.row rather than start[0]. """
     row: int
     column: int
+
+   # def __str__(self) -> str:
+   #     return f"{self.row}, {self.column}"
 
 class Maze:
     """ Defines a Maze. Note all variables are private except start. """
     def __init__(self, rows: int = 10, columns: int = 10, sparseness: float = 0.2,
         start: MazeLocation = MazeLocation(0, 0),
         goal: MazeLocation = MazeLocation(9, 9)) -> None:
+
         # Initialize basic instance variables
-        self._row: int = rows
+        self._rows: int = rows
         self._columns: int = columns
         self.start: MazeLocation = start
         self.goal: MazeLocation = goal
@@ -63,7 +69,7 @@ class Maze:
         return ml == self.goal
 
     def successor(self, ml: MazeLocation) -> List[MazeLocation]:
-        """ Find all the possible next locations for a given MazeLocation. 
+        """ Find all the possible next locations for a given MazeLocation.
             First check within the maze then check that it is not blocked. """
         locations: List[MazeLocation] = []
 
@@ -76,12 +82,48 @@ class Maze:
             locations.append(MazeLocation(ml.row, ml.column + 1))
         if ml.column - 1 >= 0 and self._grid[ml.row][ml.column - 1] != Cell.BLOCKED:
             locations.append(MazeLocation(ml.row, ml.column - 1))
-
+        #print(f"=> {locations}")
         return locations
 
+    def mark(self, path: List[MazeLocation]):
+        """ Mark the maze with the successful path. """
+        for maze_location in path:
+            self._grid[maze_location.row][maze_location.column] = Cell.PATH
+
+        self._grid[self.start.row][self.start.column] = Cell.START
+        self._grid[self.goal.row][self.goal.column] = Cell.GOAL
+
+    def clear(self, path: List[MazeLocation]):
+        """ Remove a path on a maze. """
+        for maze_location in path:
+            self._grid[maze_location.row][maze_location.column] = Cell.EMPTY
+
+        self._grid[self.start.row][self.start.column] = Cell.START
+        self._grid[self.goal.row][self.goal.column] = Cell.GOAL
 
 if __name__ == "__main__":
-    maze: Maze = Maze()
-    print(maze)
 
+    # Test DFS
+    m: Maze = Maze()            # Use defaults
+    print("\n=> New Maze\n")
+    print(m)
 
+    solution1: Optional[Node[MazeLocation]] = dfs(m.start, m.goal_test, m.successor)
+    if solution1 is None:
+        print("\n => No solution found using DFS.")
+    else:
+        path1: List[MazeLocation] = node_to_path(solution1)
+        m.mark(path1)
+        print(m)
+        m.clear(path1)
+
+    # BFS
+    solution2: Optional[Node[MazeLocation]] = bfs(m.start, m.goal_test, m.successor)
+
+    if solution2 is None:
+        print("\n => No solution found in BFS!")
+    else:
+        path2: List[MazeLocation] = node_to_path(solution2)
+        m.mark(path2)
+        print(m)
+        m.clear(path2)
