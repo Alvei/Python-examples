9177476e190d6bf793b7916a35146d8bf417df3a
alvei <hugo_sarrazin@yahoo.com>
2020-01-31 16:38:29 -0800

    Creation

---
+++ David_Kopec/chap3_csp.py
@@ -0,0 +1,77 @@
+""" Constraints-statisfaction problems core functionality.
+    The default implementation should be overridden.
+    Because we are defining the Constraint class as an
+    abstract class it will never be instantiated
+    Inspired by David Kopec.  """
+from typing import Generic, TypeVar, Dict, List, Optional
+from abc import ABC, abstractmethod
+V = TypeVar('V ')  # Variable type
+D = TypeVar('D ')  # domain type
+
+class Constraint(Generic[V, D], ABC):
+    """ An asbstract class that will be overridded. """
+    def __init__(self, variables: List[V]) -> None:
+        self.variables = variables
+
+    # Must be overridden by subclasses
+    @abstractmethod
+    def satisfied(self, assignment: Dict[V, D]) -> bool:
+        ...
+
+""" A constraint satisfaction problem consists of variables of type V
+    that have ranges of values known as domains of type D and constraints
+    that determine whether a particular variable's domain selection is valid. """
+class CSP(Generic[V, D]):
+    def __init__(self, variables: List[V], domains: Dict[V, List[D]]) -> None:
+        """ Initialize instance of class and in particular the constraints dictionary. """
+        self.variables: List[V] = variables         # Variables to be constrained
+        self.domains: Dict[V, List[D]] = domains    # Domain of each variable
+
+        self.contraints: Dict[V, List[Constraint[V, D]]] = {}
+        for variable in self.variables:
+            self.contraints[variable] = []
+            if variable not in self.domains:
+                raise LookupError("Every variable should have a domain assigned to it.")
+
+    def add_constraint(self, constraint: Constraint[V, D]) -> None:
+        """ Goes through all the variable touched by a specific constraint and
+            add itself to the contraints maping for each of them. """
+        for variable in constraint.variables:
+            if variable not in self.variables:
+                raise LookupError("Variable in constraint no in CSP.")
+            else:
+                self.contraints[variable].append(constraint)
+
+    def consistent(self, variable: V, assignment: Dict[V, D]) -> bool:
+        """ Check if the value assignment is consistent by checking
+            all constraints for the variable against it. """
+        for constraint in self.constraints[variable]:
+            if not constraint.satisfied(assignment):
+                return False
+        return True             # default case
+
+    def backtracking_search(self, assignment: Dict[V, D] = {}) -> Optional[Dict[V,D]]:
+        """ XXX """
+
+        # Assignment is complete if every variable is assigned - our base case.
+        if len(assignment) == len(self.variables):
+            return assignment
+
+        # Get all variables in the CSP but not in the assignment
+        unassigned: List[V] =  [v for v in self.variables if v not in assignment]
+
+        # Get every possible domain value of the first unassigned variable
+        first: V = unassigned[0]
+
+        for value in self.domains[first]:
+            local_assignment = assignment.copy()
+            local_assignment[first] = value
+
+            # If we're still consistent, recurse (continue)
+            if self.consistent(first, local_assignment):
+                result: Optional[Dict[V, D]] = self.backtracking_search(local_assignment)
+
+            # If we did not find the result, we will end-up backtracking
+            if result is not None:
+                return result
+        return None # default
