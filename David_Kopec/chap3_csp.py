74e0225c7dcff25eee790ed7638af21126507180
alvei <hugo_sarrazin@yahoo.com>
2020-02-01 08:17:38 -0800

    # Updated comments

--- David_Kopec/chap3_csp.py
+++ David_Kopec/chap3_csp.py
@@ -1,32 +1,42 @@
-""" Constraints-statisfaction problems core functionality.
-    The default implementation should be overridden.
-    Because we are defining the Constraint class as an
-    abstract class it will never be instantiated
-    Inspired by David Kopec.  """
+""" Constraints-Statisfaction Problems (CSP) core functionality.
+    CSPs are composed of variables with possible values that fall into ranges known
+    as domains. Contraints between the variables must be satisfied.
+
+    Inspired by David Kopec. Chapter 3.  """
+
 from typing import Generic, TypeVar, Dict, List, Optional
 from abc import ABC, abstractmethod
-V = TypeVar('V ')  # Variable type
-D = TypeVar('D ')  # domain type
+V = TypeVar('V')  # Variable type
+D = TypeVar('D')  # Domain type. This will be the range of a variable
 
 class Constraint(Generic[V, D], ABC):
-    """ An asbstract class that will be overridded. """
+    """ The default implementation the constraint is an abstract class
+        that will be overridded. The abstract class will never be instantiated.
+        It includes a satisfied method to determine if contraint is satisfied"""
     def __init__(self, variables: List[V]) -> None:
+        """ Initialized with a list of variables. """
         self.variables = variables
 
     # Must be overridden by subclasses
     @abstractmethod
     def satisfied(self, assignment: Dict[V, D]) -> bool:
+        """ Empty function that will overidden. """
         ...
 
-""" A constraint satisfaction problem consists of variables of type V
-    that have ranges of values known as domains of type D and constraints
-    that determine whether a particular variable's domain selection is valid. """
 class CSP(Generic[V, D]):
+    """ A Constraint Satisfaction Problem (CSP) consists of variables of type V
+        that have ranges of values known as domains of type D and constraints
+        that determine whether a particular variable's domain selection is valid. """
+
     def __init__(self, variables: List[V], domains: Dict[V, List[D]]) -> None:
-        """ Initialize instance of class and in particular the constraints dictionary. """
+        """ Initialize instance of class and in particular the constraints dictionary.
+            domains is a Dict mapping variables to lists of possible values.
+            constraints is a Disct that maps each variable to a list of contraints. """
         self.variables: List[V] = variables         # Variables to be constrained
         self.domains: Dict[V, List[D]] = domains    # Domain of each variable
 
+        # Create an empty list of contraints for each variables.
+        # Constraints on a variables will be created by add_constraint()
         self.constraints: Dict[V, List[Constraint[V, D]]] = {}
         for variable in self.variables:
             self.constraints[variable] = []
@@ -51,22 +61,31 @@ class CSP(Generic[V, D]):
         return True             # default case
 
     def backtracking_search(self, assignment: Dict[V, D] = {}) -> Optional[Dict[V, D]]:
-        # assignment is complete if every variable is assigned (our base case)
+        """ Core backtracking search algorithm. Once you hit a wall in your search,
+            you go back to the last known point where you made a decision before the wall,
+            and choose a different path. It is a recursive depth-first algo. """
+
+        # Assignment is complete if every variable is assigned (our base case for recursive search)
         if len(assignment) == len(self.variables):
             return assignment
 
-        # get all variables in the CSP but not in the assignment
+        # Get all variables in the CSP but not in the assignment
         unassigned: List[V] = [v for v in self.variables if v not in assignment]
-
-        # get the every possible domain value of the first unassigned variable
+        # Get every possible domain value of the first unassigned variable
         first: V = unassigned[0]
+
+        # Loop through the domain values for that variable. Create temp copy of assignment
         for value in self.domains[first]:
             local_assignment = assignment.copy()
             local_assignment[first] = value
-            # if we're still consistent, we recurse (continue)
+
+            # We call .consistent() method. if we're still consistent, we recurse (continue)
+            # .consistent() method will check all the contraints for that variable
             if self.consistent(first, local_assignment):
+                # Pass the local_assignment to the .backtracking_search() method
                 result: Optional[Dict[V, D]] = self.backtracking_search(local_assignment)
-                # if we didn't find the result, we will end up backtracking
+
+                # If we didn't find the result, we will end up backtracking
                 if result is not None:
                     return result
         return None
--- David_Kopec/map_coloring.py
+++ David_Kopec/map_coloring.py
@@ -1,27 +1,44 @@
-""" Inspired by David Kopec. """
+""" Map Colloring constraint problem.
+    The constraints are that no two adjacent regions can be colored
+    with the same color. So the condition depends on who borders who.
+    It is therefore a binary contraints (between two variables).
+    Every two regions that share a border will have a binary constraints meaning
+    they cannot have the same color.
+    Inspired by David Kopec. """
 
-from chap3_csp import Constraint, CSP
 from typing import Dict, List, Optional
+from chap3_csp import Constraint, CSP
 
 class MapColoringConstraint(Constraint[str, str]):
+    """ Define the map. It takes Constraint object as input. """
+
     def __init__(self, place1: str, place2: str) -> None:
+        """ Initializer. super() is used to call method from the superclass. """
         super().__init__([place1, place2])
-        self.place1: str = place1
-        self.place2: str = place2
+        self.place1: str = place1  # 1st region that border the 2nd region
+        self.place2: str = place2  # inverse
 
     def satisfied(self, assignment: Dict[str, str]) -> bool:
+        """ Overrides the abstractmethod. """
         if self.place1 not in assignment or self.place2 not in assignment:
             return True
 
         return assignment[self.place1] != assignment[self.place2]
 
 if __name__ == "__main__":
+
+    # Name of the regions
     variables: List[str] = ["Western Australia", "Northern Territory", "South Australia",
                             "Queensland", "New South Wales", "Victoria", "Tasmania"]
+
+    # Create the domains. For each regions, assign a list of potential solutions
     domains: Dict[str, List[str]] = {}
     for variable in variables:
         domains[variable] = ["red", "green", "blue"]
+
     csp: CSP[str, str] = CSP(variables, domains)
+
+    # Define the binary constraints or simply which region borders another region
     csp.add_constraint(MapColoringConstraint("Western Australia", "Northern Territory"))
     csp.add_constraint(MapColoringConstraint("Western Australia", "South Australia"))
     csp.add_constraint(MapColoringConstraint("South Australia", "Northern Territory"))
@@ -32,7 +49,10 @@ if __name__ == "__main__":
     csp.add_constraint(MapColoringConstraint("Victoria", "South Australia"))
     csp.add_constraint(MapColoringConstraint("Victoria", "New South Wales"))
     csp.add_constraint(MapColoringConstraint("Victoria", "Tasmania"))
+
+    # Run the constraint optimization algo
     solution: Optional[Dict[str, str]] = csp.backtracking_search()
+
     if solution is None:
         print("No solution found!")
     else:
