487f8554b3dd6e1ad08a2c0a9f882d2d03acf3dc
alvei <hugo_sarrazin@yahoo.com>
2020-05-04 09:53:36 -0700

    created unit test for parse_dates

---
+++ Automate_boring_stuff/read_file.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+import re
+from os.path import dirname, join
+from typing import Union
+
+def read_file(file_name: str, debug: bool = False) -> str:
+    """ Read a file and return a long string while doing some basic error checking. """
+
+    # Find the location of the current directory. Assumes the text file is in the same directory
+    current_dir = dirname(__file__)
+    file_path = join(current_dir, "./", file_name)
+
+    with open(file_path, 'rt') as in_file:
+        try:
+            # Read the entire file into a string variable
+            contents = in_file.read()
+            if debug:
+                print("Original file:\n", contents, '\n', sep="")
+            return contents
+        except IOError:
+            print(f"Unable to load {file_path} Check that it exists.")
+            return "IOError"
---
+++ Automate_boring_stuff/tests/test_parse_dates.py
@@ -0,0 +1,30 @@
+"""test_chap9_backup2zip.py """
+import pytest
+import datetime
+import sys, os
+# required to allow to find the file to test in directory above the test directory
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../')
+from Parse_dates import try_parsing_date
+
+@pytest.mark.parametrize('date, result',[
+    ('2020-05-18', datetime.datetime(2020, 5, 18)),
+    ('18.5.2014', datetime.datetime(2014, 5, 18)),
+    ('18/05/2019', datetime.datetime(2019, 5, 18)),
+])
+def test_try_parsing_date_good(date, result):
+    """check that the entered data is correct"""
+    assert try_parsing_date(date) == result
+
+@pytest.mark.parametrize('date, result',[
+    ('2020+05-18', False),
+    ('33-5-2014', False),
+    ('18-15-2019', False),
+    ('xx-xx-xxxx', False),
+    ('xxx', False)
+])
+def test_try_parsing_date_bad(date, result):
+    """check that the entered data is correct"""
+    assert try_parsing_date(date) == result
+
+
