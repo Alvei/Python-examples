ded7a9817a17b92f1fe010002aadca524445a54d
alvei <hugo_sarrazin@yahoo.com>
2020-05-10 21:44:34 -0700

    Move Parse_dates

--- Automate_boring_stuff/Chapter 7 - Regular Expressions/strong_password.py
+++ Automate_boring_stuff/Chapter 7 - Regular Expressions/strong_password.py
@@ -1,12 +1,12 @@
-""" Chapter_7_phone_and_email.py
+""" strong_password.py
     Use Regex to extract all the NA phone numbers and emails from a file.
     https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch04s02.html
-    also Inspired from Automate the Boring Stuff with Python - Al Sweigart. Chapter 6. Page 177.
+    Inspired from Automate the Boring Stuff with Python - Al Sweigart. Chapter 6. Page 177.
 """
 import re
 
 def chk_pwd(text: str) -> bool:
-    """ Check if a password is strong. To be strong you need to be at least 8 character, to have
+    """ Check if a password is strong? To be strong you need to be at least 8 character, to have
         at least one upper case, one lower case and one digit, must have at least one of [._%+-].
         Note I was not able to get the weird test to work with !."""
 
