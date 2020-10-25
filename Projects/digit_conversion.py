a4ec8bce953355881c9ace2a70238ee310018ab2
Alvei <hugo_sarrazin@yahoo.com>
2020-10-25 08:58:54 -0700

    Deleted Count folder

--- Projects/digit_conversion.py
+++ Projects/digit_conversion.py
@@ -5,22 +5,22 @@ Python 3.5 """
 
 def int_to_str(number: int) -> str:
     """Returns a decimal string representation of number."""
-    assert number >= 0   # Works only wiht positive number
+    assert number >= 0
 
-    digits = '0123456789'
-    if number == 0:          # base case
-        return '0'
+    digits = "0123456789"
+    if number == 0:  # base case
+        return "0"
 
-    result = ''
+    result = ""
     while number > 0:
         result = digits[number % 10] + result
-        number = number // 10       # Remove the last digit
+        number = number // 10  # Remove the last digit
     return result
 
 
 def add_digits(number: int) -> int:
     """ Returns the sum of the digit in number."""
-    assert number >= 0   # Works only wiht positive n
+    assert number >= 0  # Works only wiht positive n
 
     # Convert the integer into a string (a list)
     string_rep = int_to_str(number)
@@ -42,8 +42,8 @@ def is_subset(my_list1: list, my_list2: list) -> bool:
         for elem2 in my_list2:
             if elem1 == elem2:
                 matched = True
-                break       # if e1 was found in L2 got to the next e1
-        if not matched:     # if e1 was not found in L2 stop function
+                break  # if e1 was found in L2 got to the next e1
+        if not matched:  # if e1 was not found in L2 stop function
             return False
     return True
 
@@ -75,25 +75,25 @@ def get_binary_rep(number: int, num_digits: int) -> str:
        Assumes n and numDigits are non-negative ints
        Returns a num_digits str that is a binary representation of n."""
 
-    assert number >= 0   # Works only with positive n
+    assert number >= 0  # Works only with positive n
 
-    result = ''
+    result = ""
     while number > 0:
         result = str(number % 2) + result
         number = number // 2
     if len(result) > num_digits:
-        raise ValueError('not enough digits')
+        raise ValueError("not enough digits")
 
     # Pad the front of the display with zeroes
     for _ in range(num_digits - len(result)):
-        result = '0' + result
+        result = "0" + result
     return result
 
 
 def gen_power_set(my_list: list) -> list:
     """Returns a list of lists that contains all the possible
        combinations of the elements of my_list."""
-    assert isinstance(my_list, list)      # Check that the input is a list
+    assert isinstance(my_list, list)  # Check that the input is a list
 
     powerset = []
     for count1 in range(0, 2 ** len(my_list)):
@@ -102,7 +102,7 @@ def gen_power_set(my_list: list) -> list:
 
         subset = []
         for index, _ in enumerate(my_list):
-            if bin_str[index] == '1':
+            if bin_str[index] == "1":
                 subset.append(my_list[index])
                 # print(binStr)
                 # print 'index = ' + str(index) + '=> ' + str(my_list[index])
@@ -114,7 +114,7 @@ def search(my_list: list, elem: int) -> bool:
     """ Brute force search of a list
         Assumes my_list and elem are the same type."""
 
-    assert isinstance(my_list, list)      # Check that the input is a list
+    assert isinstance(my_list, list)  # Check that the input is a list
 
     for index, _ in enumerate(my_list):
         if my_list[index] == elem:
@@ -142,6 +142,7 @@ def main():
 
     num = 2
     print(f"\nIs {num} part of {list2}?: {search(list2, num)}")
+
 
 if __name__ == "__main__":
     main()
