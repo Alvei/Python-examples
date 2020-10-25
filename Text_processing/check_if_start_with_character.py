a151753768d87ec491d53565e6f3119b2295791b
Alvei <hugo_sarrazin@yahoo.com>
2020-10-24 19:28:11 -0700

    "Removed project convert_string"

--- Text_processing/check_if_start_with_character.py
+++ Text_processing/check_if_start_with_character.py
@@ -1,14 +1,15 @@
 """
-Check to see if a character is at beginning of string
-and return 1 if true
+
 Created on Wed Sep 19 21:18:07 2018. """
 
 
 def char_at_beg(string: str, char: str) -> bool:
-    """ Signature: (str, str) -> boolean"""
+    """ Check to see if a character is at beginning of string
+        and return 1 if true.
+    """
     if string.lower().startswith(char):
         return 1
     return 0
 
 
-print(char_at_beg("xoxo", 'x'))
+print(char_at_beg("xoxo", "x"))
