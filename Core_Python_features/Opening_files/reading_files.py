85b76c1e764bc41a95770e9cd8e62ddfa13d5c9d
Alvei <hugo_sarrazin@yahoo.com>
2020-10-31 12:18:03 -0700

    Added try/except error checking to reading_files.py

--- Core_Python_features/Opening_files/reading_files.py
+++ Core_Python_features/Opening_files/reading_files.py
@@ -5,7 +5,7 @@ Created on Sun Mar 11 21:29:01 2018
 https://www.computerhope.com/issues/ch001721.htm
 """
 from os.path import dirname, join
-import csv
+import csv, sys
 import numpy as np
 
 FILENAME = "lorem.txt"
@@ -13,48 +13,59 @@ FILENAME2 = "weight-data.txt"
 
 
 def read_file1(filename: str) -> str:
-    """ No error checking."""
-
-    in_file = open(filename, "rt")  # open file. 'rt' means read text data."
-    contents = in_file.read()  # read the entire file into a string variable
-    in_file.close()
-    return contents
+    """ Read the entire file into a string variable."""
+    try:
+        in_file = open(filename, "rt")  # open file. 'rt' means read text data."
+        contents = in_file.read()
+        in_file.close()
+        return contents
+    except IOError:
+        print(f"\n*** File {filename} is unavailable. ***")
+        sys.exit(1)
 
 
 def read_file2(filename: str) -> str:
-    """No error checking.
-    It opens, operates, & closes the file. All in one easy-to-read block of code.
+    """WITH creates context to open, operate, & close the file. All in one easy-to-read block of code.
     The file is automatically closed when the code block completes."""
-    with open(filename, "rt") as in_file:
-        # Read the entire file into a variable named contents.
-        contents = in_file.read()
-    return contents
+    try:
+        with open(filename, "rt") as in_file:
+            # Read the entire file into a variable named contents.
+            contents = in_file.read()
+        return contents
+    except IOError:
+        print(f"\n*** File {filename} is unavailable. ***")
+        sys.exit(1)
 
 
 def read_file3(filename: str) -> None:
-    """No error checking. Read one line at a time and print it.
+    """Read one line at a time and print it.
     The file object is an iterator. Therefore, you can use a for loop to operate on a file object
     repeatedly, and each time the same operation is performed, you'll receive a different, or next, result."""
 
-    content = []
-    with open(filename, "rt") as in_file:
-        for line in in_file:  # Store each line in a string variable "line"
-            print(line)  # prints that line
-            content.append(line)
-    print(f"Done 3: {content}")
+    try:
+        content = []
+        with open(filename, "rt") as in_file:
+            for line in in_file:  # Store each line in a string variable "line"
+                print(line)  # prints that line
+                content.append(line)
+        print(f"Done 3: {content}")
+    except IOError:
+        print(f"\n*** File {filename} is unavailable. ***")
+        sys.exit(1)
 
 
 def read_file4(filename: str) -> None:
-    """No error checking. Read line by line and remove EOL.
+    """Read line by line and remove EOL.
     To remove the line separator, use append() but now we have a list."""
-
-    lines = []
-    with open(
-        filename, "rt"
-    ) as in_file:  # Open file lorem.txt for reading of text data.
-        for line in in_file:  # Store each line in a string variable "line"
-            lines.append(line)
-        print(lines)
+    try:
+        lines = []
+        with open(filename, "rt") as in_file:
+            for line in in_file:  # Store each line in a string variable "line"
+                lines.append(line)
+            print(lines)
+    except IOError:
+        print(f"\n*** File {filename} is unavailable. ***")
+        sys.exit(1)
 
     # We can print each elements and special version of print to remove the EOL
     for element in lines:
@@ -134,16 +145,20 @@ def read_file9(filename: str) -> str:
 
 
 def read_file11(filename: str) -> list:
-    """No error checking. Read one line at a time and print it.
+    """Read one line at a time and print it.
     File object is an iterator. Therefore, you can use a for loop to operate on a file object
     repeatedly, and each time the same operation is performed, you'll receive a different, or next, result."""
 
-    content = []
-    with open(filename, "rt") as in_file:
-        for line in in_file:  # Store each line in a string variable "line"
-            print(line)  # prints that line
-            content.append(line.rstrip("\n"))
-    return content
+    try:
+        content = []
+        with open(filename, "rt") as in_file:
+            for line in in_file:  # Store each line in a string variable "line"
+                print(line)  # prints that line
+                content.append(line.rstrip("\n"))
+        return content
+    except IOError:
+        print(f"\n*** File {filename} is unavailable. ***")
+        sys.exit(1)
 
 
 def main():
@@ -152,14 +167,14 @@ def main():
     current_dir = dirname(__file__)
     file_path = join(current_dir, "./", file_name)
 
-    """print("\n1st read_file:", read_file1(file_path))
+    # print("\n1st read_file:", read_file1(file_path))
     # print("\n2nd read_file:", read_file2(file_path))
     print("\n3rd read_file:")
     read_file3(file_path)
     print("\n4th read_file:")
     read_file4(file_path)
     print("\n5th read_file:")
-    read_file5(file_path)"""
+    read_file5(file_path)
 
     file_name = FILENAME2
     file_path = join(current_dir, "./", file_name)
