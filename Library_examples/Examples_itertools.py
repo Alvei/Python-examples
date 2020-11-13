cef60f7a3fa4c1f92dd8efba8e4049d82e727bf5
Alvei <hugo_sarrazin@yahoo.com>
2020-11-12 16:28:42 -0800

    Pyling etc clean-up on two itertools

--- Library_examples/Examples_itertools.py
+++ Library_examples/Examples_itertools.py
@@ -1,13 +1,13 @@
 """ Series of examples using functions from itertools.
 An iterable: object that can return an ITERATOR. It has an __iter__ method that returns an iterator.
              Anything you can loop over with a for loop. Sequences are very commmon iteratbles
-             (e.g., list, tuples and strings). Sequences have a unique features: they can be indexed starting at 0,
-             they have a length and they can be sliced.
+             (e.g., list, tuples and strings). Sequences have a unique features: they can be
+             indexed starting at 0, they have a length and they can be sliced.
              Sets, dictionaries, files and ITERATORS are also iterables but not sequences.
 
-An iterator: object with a __next__ method. It has a state and once consumed they are gone. The only thing you
-             can do with iterator is ask them for the next item using __next__.
-             Generators, zip, map, filter, file objiects are iterators.
+An iterator: object with a __next__ method. It has a state and once consumed they are gone.
+             The only thing you can do with iterator is ask them for the next item using
+             __next__. Generators, zip, map, filter, file objiects are iterators.
              They have no length and cannot be indexed.
 
 multiple assignment, star expression and many built-in functions rely on iterators.
@@ -22,12 +22,14 @@ https://towardsdatascience.com/tour-of-python-itertools-2af84db18a5e
 https://medium.com/fintechexplained/advanced-python-itertools-library-the-gem-of-python-language-99da37dfcca2
 """
 import sys
+import itertools as it
+import operator
+import time
 from typing import Iterable, Iterator
 
 
 def try_zip_longest() -> None:
     """ Examples. """
-    from itertools import zip_longest
 
     names = "Pierre Marc Ottawa Eric Martin Ozzy".split()
     countries = "Australia Canada Global France".split()
@@ -39,30 +41,28 @@ def try_zip_longest() -> None:
     print(f"First: {ans}")
 
     # Adding  Name in shortest Iterator = countries
-    ans = list(zip_longest(names, countries))
+    ans = list(it.zip_longest(names, countries))
     print(f"Second: {ans}")
 
     # Same but with a fillvalue other than None
-    ans = list(zip_longest(names, countries, fillvalue="Worldwide"))
+    ans = list(it.zip_longest(names, countries, fillvalue="Worldwide"))
     print(f"Third: {ans}")
 
 
 def try_repeat():
     """ Repeat an item. """
-    from itertools import repeat
 
     to_repeat = "hello"
     how_many_times = 5
-    my_repeater = repeat(to_repeat, how_many_times)
+    my_repeater = it.repeat(to_repeat, how_many_times)
     print(f"type repeat object: {type(my_repeater)}")
     for i in my_repeater:
         print(i)
 
 
 def try_compress() -> None:
-    """ Takes iterable and boolean selector and outputs items
-    of the iterable where the corresponding element in the selector is True. """
-    from itertools import compress
+    """Takes iterable and boolean selector and outputs items
+    of the iterable where the corresponding element in the selector is True."""
 
     dates = [
         "2020-01-01",
@@ -80,7 +80,7 @@ def try_compress() -> None:
 
     # Convert second sequence to boolean
     bools = [n > 3 for n in counts]
-    ans = compress(dates, bools)
+    ans = it.compress(dates, bools)
     print(f"type for compress: {type(ans)}")
     print(list(ans))  # Compress returns iterator!
     #  ['2020-02-04', '2020-01-24', '2020-02-10', '2020-02-15']
@@ -88,119 +88,93 @@ def try_compress() -> None:
 
 def try_accumulate() -> None:
     """ This function accumulates results of some (binary) function. """
-    from itertools import accumulate
-    import operator
 
     data = [3, 4, 1, 3, 5, 6, 9, 0, 1]
     print(data)
 
-    ans = list(accumulate(data, max))  # running maximum
+    ans = list(it.accumulate(data, max))  # running maximum
     #  [3, 4, 4, 4, 5, 6, 9, 9, 9]
     print(ans)
 
-    ans2 = list(accumulate(range(1, 11), operator.mul))  # Factorial
+    ans2 = list(it.accumulate(range(1, 11), operator.mul))  # Factorial
     #  [1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 36
     print(ans2)
 
 
 def try_chain():
-    """ This method lets us create an iterator that returns elements from
-        all of the input iterables in a sequence until there are no elements left.
-        Hence, it can treat consecutive sequences as a single sequence. """
+    """This method lets us create an iterator that returns elements from
+    all of the input iterables in a sequence until there are no elements left.
+    Hence, it can treat consecutive sequences as a single sequence."""
 
-    from itertools import chain
-
-    chains = chain([1, 2, 3], ["a", "b", "c"], ["End"])
-    for chain in chains:
-        print(chain)
+    chains = it.chain([1, 2, 3], ["a", "b", "c"], ["End"])
+    for index in chains:
+        print(index)
 
 
 def try_cyle() -> None:
-    """ """
-    # Cycling through players
-    from itertools import cycle
+    """ Cycles through an iterable. """
 
-    players = ["John", "Ben", "Martin", "Peter"]
-
-    next_player = cycle(players).__next__
-    player = next_player()
-    #  "John"
-
-    player = next_player()
-    #  "Ben"
-    #  ...
-
-    # Infinite Spinner
-    import time
-
-    for c in cycle("/-\|"):
-        print(c, end="\r")
+    for cyc in it.cycle("/-\|"):
+        print(cyc, end="\r")
         time.sleep(0.2)
 
 
 def pairwise(iterable: Iterable) -> Iterator:
-    """ tee creates multiple iterators from one, which allows us to remember what happened.
-    s -> (s0, s1), (s1, s2), (s2, s3), ...
-    """
-    from itertools import tee
+    """tee creates multiple iterators from one, which allows us to remember what happened.
+    s -> (s0, s1), (s1, s2), (s2, s3), ..."""
 
-    a, b = tee(iterable, 2)
-    next(b, None)
-    return zip(a, b)
+    var1, var2 = it.tee(iterable, 2)
+    next(var2, None)
+    return zip(var1, var2)
 
 
 def try_tee():
-    """ Split an iterable and generate new iterables from the input.
-    The output is also an iterator that returns the iterable for the given number of items. """
-    from itertools import tee
+    """Split an iterable and generate new iterables from the input.
+    The output is also an iterator that returns the iterable for the given number of items."""
 
     iterable = ["FM", "AM"]
-    new_iterable = tee(iterable, 5)
+    new_iterable = it.tee(iterable, 5)
     for i in new_iterable:
         print(list(i))
 
 
 def try_dropwhile():
-    """ Pass an iterable along with a condition and this function will start evaluating
-        the condition on each of the elements until the condition returns False for an element.
-        As soon as the condition evaluates to False for an element, this function will then
-        return the rest of the elements of the iterable. """
-    from itertools import dropwhile
+    """Pass an iterable along with a condition and this function will start evaluating
+    the condition on each of the elements until the condition returns False for an element.
+    As soon as the condition evaluates to False for an element, this function will then
+    return the rest of the elements of the iterable."""
 
     jobs = ["job1", "job2", "job3", "job10", "job4", "job5"]
-    ans = dropwhile(lambda x: len(x) == 4, jobs)
+    ans = it.dropwhile(lambda x: len(x) == 4, jobs)
     print(f"type dropwhile: {type(ans)}")
     for i in ans:
         print(i)
 
 
 def try_takewhile():
-    """ returns all of the elements of an iterable until the first condition returns False
-        and then it does not return any other element. """
-    from itertools import takewhile
+    """returns all of the elements of an iterable until the first condition returns False
+    and then it does not return any other element."""
 
     jobs = ["job1", "job2", "job3", "job10", "job4", "job5"]
-    ans = takewhile(lambda x: len(x) == 4, jobs)
+    ans = it.takewhile(lambda x: len(x) == 4, jobs)
     for i in ans:
         print(i)
 
 
 def try_groupby():
-    """ Constructs an iterator after grouping the consecutive elements of an iterable.
-        The function returns an iterator of key, value pairs where the key is the group key and
-        the value is the collection of the consecutive elements that have been grouped by the key. """
-    from itertools import groupby
+    """Constructs an iterator after grouping the consecutive elements of an iterable.
+    The function returns an iterator of key, value pairs where the key is the group key and
+    the value is the collection of the consecutive elements that have been grouped by the key."""
 
     iterable = "FFFAARRHHHAADDMMAAALLIIKKK"
-    my_groupby = groupby(iterable)
+    my_groupby = it.groupby(iterable)
     for key, group in my_groupby:
         print("Key:", key)
         print("Group:", list(group))
 
 
 def try_islice():
-    """ """
-    from itertools import islice
+    """ Slice any iteratable. """
 
     log_file = ["first line", "second line", "third line", "fourth line"]
 
@@ -211,18 +185,17 @@ def try_islice():
         print(line)
 
     # Lazy implementation to grab first 3 lines
-    subset = islice(log_file, 3)
+    subset = it.islice(log_file, 3)
     for line in subset:
         print(line)
 
 
 def try_permutations():
-    """ create an iterator that returns successive permutations of elements in the input iterable. """
-    from itertools import permutations
+    """ create an iterator that returns successive permutations of elements in input iterable. """
 
     iterable = "FM1"
     length = 3
-    permutations = permutations(iterable, length)
+    permutations = it.permutations(iterable, length)
 
     print(f"Iterable: {iterable}")
     for permutation in permutations:
@@ -230,13 +203,12 @@ def try_permutations():
 
 
 def try_combinations():
-    """ Construct an iterator to return sub-sequences of elements of a given length.
-        Treat elements as unique based on their position and only the distinct elements are returned."""
-    from itertools import combinations
+    """Construct an iterator to return sub-sequences of elements of a given length.
+    Treat elements as unique based on their position and only the distinct elements are returned."""
 
     iterable = "FM1"
     length = 2
-    combinations = combinations(iterable, length)
+    combinations = it.combinations(iterable, length)
 
     print(f"Iterable: {iterable}")
     for combination in combinations:
@@ -245,22 +217,21 @@ def try_combinations():
 
 def main():
     """ Test harness. """
-    from itertools import repeat
 
-    lots_of_fours = repeat(4, times=100_000_000)
+    lots_of_fours = it.repeat(4, times=100_000_000)
     lots_of_fours_long = [4] * 100_000_000
-    print(f"repeat: {type(lots_of_fours)} size: {sys.getsizeof(lots_of_fours)}")
+    print(f"\nrepeat: {type(lots_of_fours)} size: {sys.getsizeof(lots_of_fours)}")
     print(f"list: {type(lots_of_fours_long)} size: {sys.getsizeof(lots_of_fours_long)}")
-    # try_zip_longest()
+    try_zip_longest()
     # try_compress()
     # try_accumulate()
     # try_islice()
 
-    ### Infinite Iterators ###
+    # ### Infinite Iterators ###
     # try_repeat()
     # try_cyle()
 
-    ### Terminating Iterators ###
+    # ### Terminating Iterators ###
     # try_chain()
     # s = ["s0", "s1", "s2", "s3"]
     # print(list(pairwise(s)))
@@ -269,7 +240,7 @@ def main():
     # try_takewhile()
     # try_groupby()
 
-    ### Combinatoric Iterators ###
+    # ### Combinatoric Iterators ###
     # try_permutations()
     try_combinations()
 
--- Library_examples/Examples_more_itertools.py
+++ Library_examples/Examples_more_itertools.py
@@ -1,4 +1,4 @@
-""" Series of examples using functions from more_itertools - not a standard library.
+""" Examples using functions from more_itertools - not a standard library.
 https://more-itertools.readthedocs.io/en/stable/
 https://martinheinz.dev/blog/16
 https://towardsdatascience.com/tour-of-python-itertools-2af84db18a5e
@@ -15,12 +15,13 @@ Covers:
         filter_except()
         sort_together()
 """
+import os
 
 
 def try_divide():
-    """ Divides iterable into number of sub-iterables while maintaining order.
-        Length of the sub-iterables might not be the same, as it
-        depends on number of elements being divided and number of sub-iterables """
+    """Divides iterable into number of sub-iterables while maintaining order.
+    Length of the sub-iterables might not be the same, as it
+    depends on number of elements being divided and number of sub-iterables"""
     from more_itertools import divide
 
     group_1, group_2 = divide(2, [1, 2, 3, 4, 5, 6])
@@ -29,15 +30,15 @@ def try_divide():
 
     data = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh"]
 
-    ans = [list(l) for l in divide(3, data)]
+    ans = [list(items) for items in divide(3, data)]
     print(f"Divide into subgroups of 3:\n{data}\nans:\n{ans}")
-    #  [['first', 'second', 'third'], ['fourth', 'fifth'], ['sixth', 'seventh']]
+    # [['first', 'second', 'third'], ['fourth', 'fifth'], ['sixth', 'seventh']]
 
 
 def try_partition():
-    """ Dividing our iterable using a predicate.
-        1- splitting list of dates into recent ones and old ones.
-        2- partitioning files based on their extension."""
+    """Dividing our iterable using a predicate.
+    1- splitting list of dates into recent ones and old ones.
+    2- partitioning files based on their extension."""
 
     from datetime import datetime, timedelta
     from more_itertools import partition
@@ -80,10 +81,11 @@ def try_partition():
 
 
 def try_consecutive_group():
-    """ we have a list of dates, where some of them are consecutive. To be able to pass these
-    dates to consecutive_groups function, we first have to convert them to ordinal numbers.
-    Then using list comprehension we iterate over groups of consecutive ordinal dates created by
-    consecutive_groups and convert them back to datetime.datetime using map and fromordinal functions."""
+    """We have a list of dates, where some of them are consecutive. To be able to
+    pass these dates to consecutive_groups function, we first have to convert
+    them to ordinal numbers.  Use list comprehension to iterate over groups of
+    consecutive ordinal dates created by consecutive_groups and convert them
+    back to datetime.datetime using map & from ordinal functions."""
 
     import datetime
     from more_itertools import consecutive_groups
@@ -98,20 +100,20 @@ def try_consecutive_group():
     ]
 
     ordinal_dates = []
-    for d in dates:
-        ordinal_dates.append(d.toordinal())
+    for date in dates:
+        ordinal_dates.append(date.toordinal())
 
     groups = [
         list(map(datetime.datetime.fromordinal, group))
         for group in consecutive_groups(ordinal_dates)
     ]
-    print(f"Consecutive Groups: ")
+    print("Consecutive Groups: ")
     for group in groups:
         print(f"{group}")
 
 
 def try_side_effects():
-    """ """
+    """ Cause side-effect when iterating over list of items."""
     from more_itertools import side_effect, consume
 
     num_events = 0
@@ -131,11 +133,10 @@ def try_side_effects():
 
 
 def try_collapse():
-    """ Allows you to flatten multiple levels of nesting. It also allows you
-        to specify base type, so that you can stop flattening with one layer
-        of lists/tuples remaining."""
+    """Allows you to flatten multiple levels of nesting. It also allows you
+    to specify base type, so that you can stop flattening with one layer
+    of lists/tuples remaining."""
     from more_itertools import collapse
-    import os
 
     # Get all nodes of tree into flat list
     tree = [
@@ -154,7 +155,7 @@ def try_collapse():
 
 
 def try_split_at():
-    """ This works like basic split for strings, but here we have iterable instead of
+    """Like basic split for strings, but here we have iterable instead of
     string and predicate function instead of delimiter."""
 
     from more_itertools import split_at
@@ -174,43 +175,51 @@ def try_split_at():
     ]
 
     ans = list(split_at(lines, lambda x: "-------------" in x))
-    #  [['erhgedrgh', 'erhgedrghed', 'esdrhesdresr', 'ktguygkyuk'], ['srdthsrdt', 'waefawef', 'ryjrtyfj'], ['edthedt', 'awefawe']]
+    #  [['erhgedrgh', 'erhgedrghed', 'esdrhesdresr', 'ktguygkyuk'],
+    # ['srdthsrdt', 'waefawef', 'ryjrtyfj'], ['edthedt', 'awefawe']]
     print(ans)
 
 
 def try_bucket():
-    """ Split iterable into multiple buckets based on some condition.
-          It creates child iterables by splitting input iterable using key function."""
+    """Split iterable into multiple buckets based on some condition.
+    Creates child iterables by splitting input iterable using key function."""
     from more_itertools import bucket
 
     class Cube:
+        """ Cube """
+
         pass
 
     class Circle:
+        """ Circle """
+
         pass
 
     class Triangle:
+        """ Triangle """
+
         pass
 
     shapes = [Circle(), Cube(), Circle(), Circle(), Cube(), Triangle(), Triangle()]
 
-    # Create a bucket object s, <more_itertools.more.bucket object at 0x7fa65323f210>
-    s = bucket(shapes, key=lambda x: type(x))
+    # Bucket object s, <more_itertools.more.bucket object at 0x7fa65323f210>
+    shape_bucket = bucket(shapes, key=lambda x: type(x))
 
-    print(f"\nCube: {list(s[Cube])}")
-    #  [<__main__.Cube object at 0x7f394a0633c8>, <__main__.Cube object at 0x7f394a063278>]
-    print(f"\nCircle: {list(s[Circle])}")
-    # [<__main__.Circle object at 0x7f394a063160>, <__main__.Circle object at 0x7f394a063198>, <__main__.Circle object at 0x7f394a063320>]
+    print(f"\nCube: {list(shape_bucket[Cube])}")
+    #  [<__main__.Cube object at 0x7f394a0633c8>,
+    # <__main__.Cube object at 0x7f394a063278>]
+    print(f"\nCircle: {list(shape_bucket[Circle])}")
+    # [<__main__.Circle object at 0x7f394a063160>,
+    # <__main__.Circle object at 0x7f394a063198>,
+    # <__main__.Circle object at 0x7f394a063320>]
 
 
 def try_map_reduce():
-    """ MapReduce implementation allows us to specify 3 functions:
-        key function (for categorizing),
-        value function (for transforming) and
-        finally reduce function (for reducing)."""
+    """Allows us to specify 3 functions: key function (for categorizing),
+    value function (for transforming) and finally reduce function."""
     from more_itertools import map_reduce
 
-    data = "This sentence has words of various lengths in it, both short ones and long ones".split()
+    data = "Sentence has words of various lengths, short ones & long ones".split()
     print(f"\nData: {data}")
 
     keyfunc = lambda x: len(x)
@@ -267,16 +276,18 @@ def try_sort_together():
     ]
 
     ans = sort_together(cols, key_list=(1, 2))  # sort based on col 1 then 2
-    #  [('Ben', 'John', 'Mary', 'Andy'), ('1985-04-01', '1994-02-06', '1998-03-14', '2000-06-25'), ('2019-03-07', '2020-01-06', '2018-08-15', '2020-01-08')]
+    #  [('Ben', 'John', 'Mary', 'Andy'), ('1985-04-01', '1994-02-06',
+    # '1998-03-14', '2000-06-25'), ('2019-03-07', '2020-01-06',
+    # '2018-08-15', '2020-01-08')]
     print(ans)
 
 
 def try_filter_except():
-    """ Let’s look at following scenario: You received mixed data, that contains both text and numbers
-        and all of it is in string form. You, however, want to work only with numbers (floats/ints) """
+    """Scenario: Received mixed data, that contains text and numb. and all
+    of in string form. You want to work only with numbers (floats/ints)."""
     from more_itertools import filter_except
 
-    data = ["1.5", "6", "not-important", "11", "1.23E-7", "remove-me", "25", "trash"]
+    data = ["1.5", "6", "not-important", "11", "1.23E-7", "remove-me", "25"]
 
     ans = map(float, filter_except(float, data, TypeError, ValueError))
     print(f"Original data: {data}")
