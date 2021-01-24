250902c2ec50716f460d5aa71b12e550528dcf0e
Alvei <hugo_sarrazin@yahoo.com>
2021-01-23 17:22:05 -0800

    two new bs4 examples

---
+++ BeautifulSoup/google_result.py
@@ -0,0 +1,54 @@
+""" google_result.py
+https://medium.com/pythoneers/5-python-intermediate-project-that-everyone-should-build-with-codes-5039f13feadb
+"""
+
+from bs4 import BeautifulSoup
+import requests
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+}
+
+
+def google(query: str) -> str:
+    """ Scrape Google. """
+    query = query.replace(" ", "+")
+    try:
+        url = f"https://www.google.com/search?q={query}&oq={query}&aqs=chrome..69i57j46j69i59j35i39j0j46j0l2.4948j0j7&sourceid=chrome&ie=UTF-8"
+        res = requests.get(url, headers=headers)
+        soup = BeautifulSoup(res.text, "html.parser")
+    except:
+        print("Make sure you have a internet connection")
+    try:
+        try:
+            ans = soup.select(".RqBzHd")[0].getText().strip()
+
+        except:
+            try:
+                title = soup.select(".AZCkJd")[0].getText().strip()
+                try:
+                    ans = soup.select(".e24Kjd")[0].getText().strip()
+                except:
+                    ans = ""
+                ans = f"{title}\n{ans}"
+
+            except:
+                try:
+                    ans = soup.select(".hgKElc")[0].getText().strip()
+                except:
+                    ans = soup.select(".kno-rdesc span")[0].getText().strip()
+
+    except:
+        ans = "can't find on google"
+    return ans
+
+
+def main():
+    """ Test harness. """
+
+    result = google(str(input("Query:\n")))
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
